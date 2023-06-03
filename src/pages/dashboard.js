a0c33b16d1d177109659d12d5d59912317ce05d6
tarisadinda20 <tarisadinda20@gmail.com>
2023-06-03 17:24:10 +0700

    add component

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,9 +1,17 @@
 import CardStatistic from "@/components/dashboard/card-statistic"
 import LayoutMain from "@/components/layouts/main"
+import CustomTable from "@/components/table"
 
 export default function Dashboard() {
     return(<>
-        <CardStatistic />
+        <div className="d-flex gap-3 mb-5">
+            <CardStatistic />
+            <CardStatistic />
+            <CardStatistic />
+        </div>
+        <div>
+            <CustomTable />
+        </div>
     </>)
 }
 
