4e00cba1886ff84768b9863a866e7a0ace9e0625
tarisadinda20 <tarisadinda20@gmail.com>
2023-06-03 17:25:10 +0700

    edit font style for material ui

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '@/styles/globals.scss'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({
+  weight: ['400', '600', '700', '800'],
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
 
+  const theme = createTheme({
+    typography: {
+      fontFamily: inter,
+    },
+  })
+
   return getLayout(<>
-    <Component {...pageProps} />
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
   </>) 
 }
