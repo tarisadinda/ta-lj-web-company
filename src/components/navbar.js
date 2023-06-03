8bb24950e4dd47b39d04a4c0258f0f522bc2213a
tarisadinda20 <tarisadinda20@gmail.com>
2023-06-03 17:24:54 +0700

    add sidebar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,30 +1,35 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import AppBar from '@mui/material/AppBar';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import * as React from 'react'
+import styles from '@/styles/components/Navbar.module.scss'
+import Box from '@mui/material/Box'
+import Drawer from '@mui/material/Drawer'
+import AppBar from '@mui/material/AppBar'
+import CssBaseline from '@mui/material/CssBaseline'
+import Toolbar from '@mui/material/Toolbar'
+import List from '@mui/material/List'
+import Typography from '@mui/material/Typography'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import InboxIcon from '@mui/icons-material/MoveToInbox'
+import MailIcon from '@mui/icons-material/Mail'
+import LogoutIcon from '@mui/icons-material/Logout'
+import PersonIcon from '@mui/icons-material/Person'
 
-const drawerWidth = 240;
+const drawerWidth = 240
 
 export default function Navbar() {
+  const [activeIndex, setActiveIndex] = React.useState(null)
+
+  const selectBtn = (index) => {
+    setActiveIndex(index)
+  }
+
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
         <Toolbar>
-          <Typography variant="h6" noWrap component="div">
-            Clipped drawer
-          </Typography>
         </Toolbar>
       </AppBar>
       <Drawer
@@ -36,11 +41,23 @@ export default function Navbar() {
         }}
       >
         <Toolbar />
-        <Box sx={{ overflow: 'auto' }}>
+        <Box sx={{ overflow: 'auto' }} className={styles.menuWrap}>
           <List>
             {['Dashboard', 'Data Pelamar', 'Cari Karyawan'].map((text, index) => (
-              <ListItem key={text} disablePadding>
+              <ListItem key={index} disablePadding onClick={() => selectBtn(index)}>
+                <ListItemButton selected={activeIndex === index}>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <List>
+            {['Profil Saya', 'Logout'].map((text, index) => (
+              <ListItem key={index} disablePadding>
                 <ListItemButton>
+                  <ListItemIcon>
+                    {text === 'Logout' ? <LogoutIcon fontSize="small" /> : <PersonIcon fontSize="small" />}
+                  </ListItemIcon>
                   <ListItemText primary={text} />
                 </ListItemButton>
               </ListItem>
