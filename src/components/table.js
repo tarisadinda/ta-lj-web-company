1f98beabc43c06f9e1c3beac301cfa63d93e9275
tarisadinda20 <tarisadinda20@gmail.com>
2023-07-02 20:46:38 +0700

    edit reusable table

--- src/components/table.js
+++ src/components/table.js
@@ -9,13 +9,11 @@ import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
 import Paper from '@mui/material/Paper'
 import IconButton from '@mui/material/IconButton'
-import EditIcon from '@mui/icons-material/Edit'
-import VisibilityIcon from '@mui/icons-material/Visibility'
 
 const CustomTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
-      backgroundColor: theme.palette.common.white,
-      color: theme.palette.common.black,
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
       fontWeight: 700
     },
     [`&.${tableCellClasses.body}`]: {
@@ -33,54 +31,42 @@ const CustomTableRow = styled(TableRow)(({ theme }) => ({
     },
 }))
 
-function createData(id, name, position, skill, date, status) {
-    return {id, name, position, skill, date, status}
-}
-
-const dummyData = [
-    createData(1, 'Riana', 'Mobile Developer', 'Level 1', '5/10/2022', 'Dalam review'),
-    createData(2, 'Riana', 'Mobile Developer', 'Level 2', '6/10/2022', 'Diterima'),
-    createData(3, 'Riana', 'Mobile Developer', 'Level 2', '6/10/2022', 'Dalam review'),
-    createData(4, 'Riana', 'Mobile Developer', 'Level 1', '8/10/2022', 'Diterima'),
-    createData(5, 'Riana', 'Mobile Developer', 'Level 2', '9/10/2022', 'Dalam review'),
-]
-
-export default function CustomTable() {
+export default function CustomTable({columns, data, actionButton}) {
     return(<>
         <TableContainer component={Paper}>
             <Table sx={{ minWidth: 700 }} aria-label="customized table">
                 <TableHead>
                     <TableRow>
-                        <CustomTableCell className={styles.column}>No</CustomTableCell>
-                        <CustomTableCell className={styles.column}>Nama Pelamar</CustomTableCell>
-                        <CustomTableCell className={styles.column}>Posisi</CustomTableCell>
-                        <CustomTableCell className={styles.column}>Keahlian</CustomTableCell>
-                        <CustomTableCell className={styles.column}>Tanggal Melamar</CustomTableCell>
-                        <CustomTableCell className={styles.column}>Status</CustomTableCell>
-                        <CustomTableCell className={styles.column}>Aksi</CustomTableCell>
+                        <CustomTableCell>No</CustomTableCell>
+                        {columns.length > 0 && columns.map((column, index) => (
+                            <CustomTableCell key={index}>{column.label}</CustomTableCell>
+                        ))}
+                        <CustomTableCell>Aksi</CustomTableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {dummyData.map((data, index) => (
-                        <CustomTableRow key={index}>
-                            <CustomTableCell>{index+1}</CustomTableCell>
-                            <CustomTableCell>{data.name}</CustomTableCell>
-                            <CustomTableCell>{data.position}</CustomTableCell>
-                            <CustomTableCell>{data.skill}</CustomTableCell>
-                            <CustomTableCell>{data.date}</CustomTableCell>
-                            <CustomTableCell>{data.status}</CustomTableCell>
-                            <CustomTableCell>
-                                <div>
-                                    <IconButton>
-                                        <EditIcon />
-                                    </IconButton>
-                                    <IconButton>
-                                        <VisibilityIcon />
-                                    </IconButton>
-                                </div>
-                            </CustomTableCell>
+                    {data !== undefined ? 
+                        data.map((item, index) => (
+                            <CustomTableRow key={index}>
+                                <CustomTableCell>{index+1}</CustomTableCell>
+                                {columns.map((col, index) => (
+                                    <CustomTableCell key={index}>{col.render(item)}</CustomTableCell>
+                                ))}
+                                <CustomTableCell>
+                                    <div className={styles.actions}>
+                                        {actionButton !== undefined && actionButton.map((btn, index) => (
+                                            <IconButton key={index} onClick={() => btn.function(item.id)}>
+                                                {btn.icon}
+                                            </IconButton>
+                                        ))}
+                                    </div>
+                                </CustomTableCell>
+                            </CustomTableRow>
+                        )) :
+                        <CustomTableRow>
+                            <CustomTableCell align="center" colSpan={10}>Belum ada data tersedia</CustomTableCell>
                         </CustomTableRow>
-                    ))}
+                    }
                 </TableBody>
             </Table>
         </TableContainer>
