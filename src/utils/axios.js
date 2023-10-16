73cb5b6f988f3d40d3c649bb03f3710117896e6a
tarisadinda20 <tarisadinda20@gmail.com>
2023-10-16 22:16:26 +0700

    add axios instance

---
+++ src/utils/axios.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import { getToken } from './token'
+
+export const axiosInstance = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    timeout: 120000,
+    headers: {
+        "Content-Type": "multipart/form-data"
+    }
+})
+
+axiosInstance.interceptors.request.use (
+    (config) => {
+        const token = getToken()
+
+        return {
+            ...config,
+            headers: {
+                ...config.headers,
+                Authorization: `Bearer ${token}`
+            }
+        }
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
