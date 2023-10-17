73711f05afd8352efd5860fbb6d0f33951c0b1ff
tarisadinda20 <tarisadinda20@gmail.com>
2023-10-17 20:18:42 +0700

    set up svgr

--- next.config.js
+++ next.config.js
@@ -8,9 +8,15 @@ const nextConfig = {
     prependData: `@import "@/styles/utils/_variables.scss";
     `,
   },
-  // images: {
-  //   unoptimized: true
-  // }
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
