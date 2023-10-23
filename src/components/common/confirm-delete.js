42dad8cd644c3b0c5107a78375005007a73a4bf1
tarisadinda20 <tarisadinda20@gmail.com>
2023-10-23 20:17:50 +0700

    add prop

--- src/components/common/confirm-delete.js
+++ src/components/common/confirm-delete.js
@@ -3,7 +3,7 @@ import cn from 'classnames'
 import { Dialog, DialogContent } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete';
 
-export default function ConfirmDeleteModal({ open, delFunc, onClose, title }) {
+export default function ConfirmDeleteModal({ open, delFunc, onClose, title, desc }) {
     return(<>
         <Dialog open={open} fullWidth maxWidth='xs'>
             <DialogContent>
@@ -11,6 +11,7 @@ export default function ConfirmDeleteModal({ open, delFunc, onClose, title }) {
                     <DeleteIcon sx={{ fontSize: 50, color: '#D41C1D' }} />
                 </div>
                 <p className={styles.text}>{title}</p>
+                {desc && <p className={styles.desc}>{desc}</p>}
                 <div className={styles.btnList}>
                     <button onClick={onClose} className={cn(styles.actBtn, "btn btn-outline gray")}>Batal</button>
                     <button onClick={delFunc} className={cn(styles.actBtn, "btn btn-primary red")}>Ya, Hapus</button>
